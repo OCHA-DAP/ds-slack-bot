d57837d5a09f6435926e814b07b9be46f9277375
hannahker <hannah.ker96@gmail.com>
2025-11-27 12:08:08 -0800

    add dataset notifications script

---
+++ src/dataset_updates.py
@@ -0,0 +1,32 @@
+import os
+
+from dotenv import load_dotenv
+from slack_sdk import WebClient
+from slack_sdk.errors import SlackApiError
+
+load_dotenv()
+
+client = WebClient(token=os.getenv("SLACK_BOT_TOKEN"))
+channel = "#hdx-signals-bot-testing"
+
+
+def send_monthly_message():
+    try:
+        client.chat_postMessage(
+            channel=channel,
+            text="""
+*<!channel> dataset updates needed!*
+
+Please update the following datasets on Azure. Instructions are linked below.
+- <https://knowledge.base.unocha.org/wiki/spaces/DSCI/pages/4829413383/Instructions+for+manual+updates#1.-EM-DAT|EM-DAT>
+- <https://knowledge.base.unocha.org/wiki/spaces/DSCI/pages/4829413383/Instructions+for+manual+updates#2.-CERF-Allocations|CERF Allocations>
+
+React with ✅ when done, for each dataset.
+            """,  # noqa: E501
+        )
+    except SlackApiError as e:
+        print(f"Error: {e.response['error']}")
+
+
+if __name__ == "__main__":
+    send_monthly_message()
